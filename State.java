cfe883568781b71335406ac4025d7cdf134dc1f5
Jack Carroll <jhc8@email.sc.edu>
2021-04-12 10:38:13 -0400

    Added interface for program

---
+++ State.java
@@ -0,0 +1,10 @@
+/**
+ * State Design Interface 
+ * @author Jackson Carroll
+ */
+public interface State {
+    public void pressHomeButton();
+    public void pressNetflixButton();
+    public void pressHuluButton();
+    public void pressMovieButton();
+}
